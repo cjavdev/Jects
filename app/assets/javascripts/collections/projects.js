c33f1c095de2e816bd5f4f3b0ef27ca592db1c3f
CJ Avilla <cjavilla@gmail.com>
2014-07-31 22:08:24 -0700

    attempt to fix sorting

--- app/assets/javascripts/collections/projects.js
+++ app/assets/javascripts/collections/projects.js
@@ -2,7 +2,9 @@
 Jects.Collections.Projects = Backbone.Collection.extend({
   model: Jects.Models.Project,
   url: 'api/projects',
-  comparator: 'votes_count'
+  comparator: function () {
+    return -this.get('votes_count');
+  }
 });
 
 Jects.projects = new Jects.Collections.Projects();
